366715f62f2624f1399597caf2f7545879a35977
David-Wobrock <david.wobrock@gmail.com>
2018-01-21 14:10:45 +0100

    Change tests manner

--- tests/test_project/test_project/wsgi.py
+++ tests/test_project/test_project/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "test_project_add_not_null_column.settings")
+                      "test_project.settings")
 
 application = get_wsgi_application()
